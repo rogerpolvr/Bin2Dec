feb18fe5b515e3b29d4e88bbaf73cae33fcd79ea
Roger Oliveira <rogerpolvr@gmail.com>
2020-02-19 19:12:20 -0300

    Work in progress

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { FaSync } from "react-icons/fa";
 import { Container, Form } from "./styles";
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaSync />
-        Binary to Decimal
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Binary value" />
-        <input type="text" placeholder="Decimal result" />
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newBinary: "",
+    newDecimal: ""
+  };
+
+  handleInputChange = e => {
+    const binaryValue = e.target.value;
+    this.setState({ newBinary: binaryValue });
+    this.setState({ newDecimal: binaryValue * 2 });
+  };
+
+  bin2dec(bin) {
+    let result;
+    for (let index = bin.lengh - 1; index >= 0; index--) {
+      result += parseInt(bin[index]) * Math.pow(2, bin.lengh - 1 - index);
+    }
+    return result;
+  }
+
+  render() {
+    const { newBinary, newDecimal } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaSync />
+          Binary to Decimal
+        </h1>
+        <Form>
+          <input
+            type="text"
+            placeholder="Binary value"
+            value={newBinary}
+            onChange={this.handleInputChange}
+          />
+          <input type="text" placeholder="Decimal result" value={newDecimal} />
+        </Form>
+      </Container>
+    );
+  }
 }
