f0ce364832e91946abfce6e866401473346cfe84
Roger Oliveira <rogerpolvr@gmail.com>
2020-02-19 17:33:22 -0300

    Setting other configs on project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./App.css";
+import Routes from "./routes";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
